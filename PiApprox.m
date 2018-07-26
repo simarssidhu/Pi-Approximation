938f83247087465a241f702da6fb122e1e19f30d
Simarjot Sidhu <41749062+simarssidhu@users.noreply.github.com>
2018-07-26 11:44:13 -0400

    Update to PiApprox.m
    Added comments for clarity

--- PiApprox.m
+++ PiApprox.m
@@ -1,7 +1,11 @@
-x = 1.01;
-Max_Iter = 1000;  % if lowering Max_Iter, delete Approx and True_Error variables in Workspace and then Run
-Exponent = 2;
-Precision = 1000000;
+%% Numerical Approximation for Pi
+
+x = 1.01;   % Initial base of exponential
+Max_Iter = 1000;  % Number of iterations
+Exponent = 2;   % Exponent of exponential
+Precision = 1000000;    % Number of digits
+
+% Algorithm for approximation
 
 for i=1:Max_Iter
     x = x^((Exponent)^(cos(x)));
@@ -9,15 +13,15 @@ for i=1:Max_Iter
     True_Error(i,1) = (pi - 2*x)/pi;
 end
 
-digits(Precision);
+digits(Precision);  % Sets number of digits to be shown
 x = x*2;
 display(vpa(x));
-diary ('pi.txt')
+diary ('pi.txt')    % Saves pi result into .txt file
 vpa(x)
 diary off
 
 Iter = 1:Max_Iter;
-plot(Iter,Approx);
+plot(Iter,Approx);  % Plots approximation of pi vs. number of iteration
 
 clear True_Error;
 clear Approx;
